411e3271979c537b623ae19e3977bab9a0ae0ab0
FatFatFat1 <48385210+FatFatFat1@users.noreply.github.com>
2022-08-13 19:52:36 +0700

    [update] coin updater
    Теперь счетчик трясется

--- Farm/Assets/Scripts/UI/ScoreUpdate.cs
+++ Farm/Assets/Scripts/UI/ScoreUpdate.cs
@@ -5,16 +5,25 @@ using UnityEngine.UI;
 
 public class ScoreUpdate : MonoBehaviour
 {
+    Vector3 truePos;
+    private void Awake()
+    {
+        truePos = gameObject.transform.position;
+    }
     public void StartAddScore(int oldScore, int currentScore, Text text)
     {
-        StartCoroutine(AddScore(oldScore , currentScore, text));
+        StartCoroutine(AddScore(oldScore, currentScore, text));
     }
-    IEnumerator AddScore(int oldScore, int currentScore , Text text)
+    IEnumerator AddScore(int oldScore, int currentScore, Text text)
     {
         for (int i = oldScore; i <= currentScore; i++)
         {
+            Vector3 randPos;
+            randPos = new Vector3(gameObject.transform.position.x + Random.Range(-0.5f, 0.5f), gameObject.transform.position.y + Random.Range(-0.5f, 0.5f), gameObject.transform.position.z + Random.Range(-0.5f, 0.5f));
+            gameObject.transform.position = randPos;
             text.text = "Money:" + i + "$";
             yield return new WaitForEndOfFrame();
         }
+        gameObject.transform.position = truePos;
     }
 }
